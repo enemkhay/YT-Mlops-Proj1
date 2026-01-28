8d37bd0c1d49961e33aaf8e7a390fdf894467170
enemkhay <khajhat2@gmail.com>
2026-01-28 14:37:27 +0700

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Minh Kha",
+    author_email="khajhat2@gmail.com",
+    packages=find_packages()
+)
